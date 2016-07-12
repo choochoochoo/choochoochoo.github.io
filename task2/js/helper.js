2cb13b3860b1a96b00cce5867e4e2c6042a7e796
choochoochoo <choochoochoo@outlook.com>
2016-07-12 12:55:54 +0300

    Create helper.js

---
+++ task2/js/helper.js
@@ -0,0 +1,25 @@
+App.Helper = {
+    getPosition: function (e) {
+        var left = 0;
+        var top = 0;
+
+        while (e.offsetParent) {
+            left += e.offsetLeft;
+            top += e.offsetTop;
+            e = e.offsetParent;
+        }
+
+        left += e.offsetLeft;
+        top += e.offsetTop;
+
+        return {x: left, y: top}
+    },
+
+    getMouseOffset: function (target, e) {
+        var docPos = App.Helper.getPosition(target);
+        //return {x: e.pageX - docPos.x, y: e.pageY - docPos.y};
+        return {x: e.pageX - docPos.x, y: e.y - docPos.y};
+    }
+};
+
+
