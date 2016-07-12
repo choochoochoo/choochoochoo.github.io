b8541a24c2ddabe743b2e2ca329366243b77235b
choochoochoo <choochoochoo@outlook.com>
2016-07-12 17:03:07 +0300

    Update door2.js

--- task2/js/door2.js
+++ task2/js/door2.js
@@ -23,14 +23,20 @@ function Door2(number, onUnlock) {
     button0.addEventListener('pointermove', _onButton0PointerMove.bind(this));
 
     function _onButton0PointerDown(event) {
-        console.log('pointerdown')
-        //event.target.classList.add('door2-riddle__button_0_captured');
-        //
-        //mouseOffset = App.Helper.getMouseOffset(event.target, event);
-        //positionButtonDefault = App.Helper.getPosition(button0);
-        //
-        //console.log(mouseOffset)
-        //console.log(positionButtonDefault)
+    //    console.log('pointerdown')
+        event.target.classList.add('door2-riddle__button_0_captured');
+
+       // event.target.style.width = event.width + "px";
+       // event.target.style.height = event.height + "px";
+
+        mouseOffset = App.Helper.getMouseOffset(event.target, event);
+        positionButtonDefault = App.Helper.getPosition(button0);
+
+
+        //var positionCircle1Default = App.Helper.getPosition(circle1);
+
+       // console.log(mouseOffset)
+       // console.log(positionCircle1Default)
     }
 
     function _onButton0PointerUp(event) {
@@ -41,20 +47,17 @@ function Door2(number, onUnlock) {
 
     function _onButton0PointerCancel(event) {
         event.target.classList.remove('door2-riddle__button_0_captured');
-        console.log('pointercancel')
+     //   console.log('pointercancel')
         resetButton0();
 
     }
 
     function _onButton0PointerLeave(event) {
         event.target.classList.remove('door2-riddle__button_0_captured');
-        console.log('pointerleave')
+      //  console.log('pointerleave')
         resetButton0();
 
     }
-
-
-
 
     function _onButton0PointerMove(event) {
         if (event.target.classList.contains('door2-riddle__button_0_captured')) {
@@ -62,17 +65,35 @@ function Door2(number, onUnlock) {
             button0.style.left = event.x - positionButtonDefault.x - mouseOffset.x + 'px';
             button0.style.top = event.y - positionButtonDefault.y - mouseOffset.y + 'px';
 
+           // console.log(document.elementFromPoint(event.clientX, event.clientY))
 
+          //  console.log(event)
         }
+
+
     }
 
-    circle2.addEventListener('pointerenter', _onCircle2PointerLeave.bind(this));
-    //circle1.addEventListener('pointerleave', _onCircle2PointerLeave.bind(this));
-    //circle0.addEventListener('pointerenter', _onCircle2PointerLeave.bind(this));
 
-    function _onCircle2PointerLeave(){
-       // console.log('pointerenter circle2')
-        resetButton0();
+    //circle0.addEventListener('pointerenter', _onCircle2PointerLeave.bind(this));
+   // circle2.addEventListener('pointerenter', _onCircle2PointerLeave.bind(this));
+
+
+    //circle1.addEventListener('pointerover', _onCircle1PointerLeave.bind(this));
+    //circle1.addEventListener('pointerenter', _onCircle1PointerLeave.bind(this));
+    //circle1.addEventListener('pointerdown', _onCircle1PointerLeave.bind(this));
+    circle1.addEventListener('pointermove', _onCircle1PointerLeave.bind(this));
+    //circle1.addEventListener('pointerup', _onCircle1PointerLeave.bind(this));
+    //circle1.addEventListener('pointercancel', _onCircle1PointerLeave.bind(this));
+    //circle1.addEventListener('pointerout', _onCircle1PointerLeave.bind(this));
+    //circle1.addEventListener('pointerleave', _onCircle1PointerLeave.bind(this));
+    //circle1.addEventListener('gotpointercapture', _onCircle1PointerLeave.bind(this));
+    //circle1.addEventListener('lostpointercapture', _onCircle1PointerLeave.bind(this));
+
+    function _onCircle1PointerLeave(event){
+       // console.log(event.type)
+       /// resetButton0();
+
+        //console.log(document.elementFromPoint(event.clientX, event.clientY))
     }
 
     function resetButton0(){
