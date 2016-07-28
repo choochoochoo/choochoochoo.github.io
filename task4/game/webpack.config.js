e5f5f06de0329250985aee70a7b2ea2427ebba7a
DSSolovev <choochoochoo@outlook.com>
2016-07-28 09:44:14 +0300

    change settings of webpack

--- task4/game/webpack.config.js
+++ task4/game/webpack.config.js
@@ -2,20 +2,20 @@ var webpack = require('webpack');
 var ExtractTextPlugin = require('extract-text-webpack-plugin');
 
 module.exports = {
-    entry: './www/js/index.js',
+    entry: './source/js/index.js',
     output: {
         filename: 'bundle.js',
         path: __dirname + '/www/public'
     },
     devtool: 'source-map',
     plugins: [
-        //new webpack.optimize.OccurrenceOrderPlugin(),
-        //new webpack.optimize.DedupePlugin(),
-        //new webpack.optimize.UglifyJsPlugin({
-        //    compressor: {
-        //        screw_ie8: true,
-        //    },
-        //}),
+        new webpack.optimize.OccurrenceOrderPlugin(),
+        new webpack.optimize.DedupePlugin(),
+        new webpack.optimize.UglifyJsPlugin({
+            compressor: {
+                screw_ie8: true,
+            },
+        }),
         new ExtractTextPlugin('bundle.css')
     ],
     module: {
