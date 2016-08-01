f1ffc504b77771de53b575b57f9c42cc30706dfe
choochoocho <choochocho@outlook.com>
2016-08-01 21:49:45 +0300

    initial commit

---
+++ faroe/index/index.bemdecl.js
@@ -0,0 +1,292 @@
+exports.blocks = [
+    {
+        "name": "page"
+    },
+    {
+        "name": "page",
+        "elems": [
+            {
+                "name": "meta"
+            }
+        ]
+    },
+    {
+        "name": "page",
+        "elems": [
+            {
+                "name": "css"
+            }
+        ]
+    },
+    {
+        "name": "page",
+        "elems": [
+            {
+                "name": "js"
+            }
+        ]
+    },
+    {
+        "name": "article"
+    },
+    {
+        "name": "section"
+    },
+    {
+        "name": "section",
+        "elems": [
+            {
+                "name": "header"
+            }
+        ]
+    },
+    {
+        "name": "section",
+        "elems": [
+            {
+                "name": "header",
+                "mods": [
+                    {
+                        "name": "picture",
+                        "vals": [
+                            {
+                                "name": "faroe"
+                            }
+                        ]
+                    }
+                ]
+            }
+        ]
+    },
+    {
+        "name": "section",
+        "elems": [
+            {
+                "name": "layout"
+            }
+        ]
+    },
+    {
+        "name": "section",
+        "elems": [
+            {
+                "name": "top-row"
+            }
+        ]
+    },
+    {
+        "name": "section",
+        "elems": [
+            {
+                "name": "vcenter"
+            }
+        ]
+    },
+    {
+        "name": "section",
+        "elems": [
+            {
+                "name": "title"
+            }
+        ]
+    },
+    {
+        "name": "section",
+        "elems": [
+            {
+                "name": "subtitle"
+            }
+        ]
+    },
+    {
+        "name": "section",
+        "elems": [
+            {
+                "name": "bottom-row"
+            }
+        ]
+    },
+    {
+        "name": "section",
+        "elems": [
+            {
+                "name": "subscription1"
+            }
+        ]
+    },
+    {
+        "name": "link"
+    },
+    {
+        "name": "link",
+        "mods": [
+            {
+                "name": "theme",
+                "vals": [
+                    {
+                        "name": "faroe"
+                    }
+                ]
+            }
+        ]
+    },
+    {
+        "name": "section",
+        "elems": [
+            {
+                "name": "subscription2"
+            }
+        ]
+    },
+    {
+        "name": "section",
+        "elems": [
+            {
+                "name": "main"
+            }
+        ]
+    },
+    {
+        "name": "section",
+        "elems": [
+            {
+                "name": "layout-text"
+            }
+        ]
+    },
+    {
+        "name": "section",
+        "elems": [
+            {
+                "name": "quote"
+            }
+        ]
+    },
+    {
+        "name": "section",
+        "elems": [
+            {
+                "name": "quote",
+                "mods": [
+                    {
+                        "name": "important",
+                        "vals": [
+                            {
+                                "name": true
+                            }
+                        ]
+                    }
+                ]
+            }
+        ]
+    },
+    {
+        "name": "section",
+        "elems": [
+            {
+                "name": "quote-content"
+            }
+        ]
+    },
+    {
+        "name": "section",
+        "elems": [
+            {
+                "name": "paragraph"
+            }
+        ]
+    },
+    {
+        "name": "section",
+        "elems": [
+            {
+                "name": "paragraph",
+                "mods": [
+                    {
+                        "name": "offset",
+                        "vals": [
+                            {
+                                "name": true
+                            }
+                        ]
+                    }
+                ]
+            }
+        ]
+    },
+    {
+        "name": "section",
+        "elems": [
+            {
+                "name": "dropcap"
+            }
+        ]
+    },
+    {
+        "name": "section",
+        "elems": [
+            {
+                "name": "quote",
+                "mods": [
+                    {
+                        "name": "bottom",
+                        "vals": [
+                            {
+                                "name": true
+                            }
+                        ]
+                    }
+                ]
+            }
+        ]
+    },
+    {
+        "name": "section",
+        "mods": [
+            {
+                "name": "offset",
+                "vals": [
+                    {
+                        "name": true
+                    }
+                ]
+            }
+        ]
+    },
+    {
+        "name": "section",
+        "elems": [
+            {
+                "name": "header",
+                "mods": [
+                    {
+                        "name": "picture",
+                        "vals": [
+                            {
+                                "name": "flight-road"
+                            }
+                        ]
+                    }
+                ]
+            }
+        ]
+    },
+    {
+        "name": "section",
+        "elems": [
+            {
+                "name": "header",
+                "mods": [
+                    {
+                        "name": "picture",
+                        "vals": [
+                            {
+                                "name": "weather"
+                            }
+                        ]
+                    }
+                ]
+            }
+        ]
+    }
+];
---
+++ faroe/index/index.deps.js
@@ -0,0 +1,269 @@
+exports.deps = [
+    {
+        "block": "page"
+    },
+    {
+        "block": "i-bem",
+        "elem": "dom"
+    },
+    {
+        "block": "jquery"
+    },
+    {
+        "block": "loader"
+    },
+    {
+        "block": "loader",
+        "mod": "type"
+    },
+    {
+        "block": "jquery",
+        "elem": "config"
+    },
+    {
+        "block": "ua"
+    },
+    {
+        "block": "objects"
+    },
+    {
+        "block": "functions"
+    },
+    {
+        "block": "dom"
+    },
+    {
+        "block": "i-bem",
+        "elem": "dom",
+        "mod": "init"
+    },
+    {
+        "block": "i-bem"
+    },
+    {
+        "block": "i-bem",
+        "elem": "internal"
+    },
+    {
+        "block": "inherit"
+    },
+    {
+        "block": "identify"
+    },
+    {
+        "block": "next-tick"
+    },
+    {
+        "block": "events"
+    },
+    {
+        "block": "page",
+        "elem": "css"
+    },
+    {
+        "block": "page",
+        "elem": "js"
+    },
+    {
+        "block": "page",
+        "elem": "meta"
+    },
+    {
+        "block": "article"
+    },
+    {
+        "block": "section"
+    },
+    {
+        "block": "link"
+    },
+    {
+        "block": "link",
+        "mod": "disabled"
+    },
+    {
+        "block": "jquery",
+        "elem": "event"
+    },
+    {
+        "block": "jquery",
+        "elem": "event",
+        "mod": "type"
+    },
+    {
+        "block": "jquery",
+        "elem": "event",
+        "mod": "type",
+        "val": "pointerclick"
+    },
+    {
+        "block": "jquery",
+        "elem": "event",
+        "mod": "type",
+        "val": "pointernative"
+    },
+    {
+        "block": "keyboard",
+        "elem": "codes"
+    },
+    {
+        "block": "control"
+    },
+    {
+        "block": "section",
+        "elem": "header"
+    },
+    {
+        "block": "section",
+        "elem": "header",
+        "mod": "picture"
+    },
+    {
+        "block": "section",
+        "elem": "layout"
+    },
+    {
+        "block": "section",
+        "elem": "top-row"
+    },
+    {
+        "block": "section",
+        "elem": "vcenter"
+    },
+    {
+        "block": "section",
+        "elem": "title"
+    },
+    {
+        "block": "section",
+        "elem": "subtitle"
+    },
+    {
+        "block": "section",
+        "elem": "bottom-row"
+    },
+    {
+        "block": "section",
+        "elem": "subscription1"
+    },
+    {
+        "block": "link",
+        "mod": "theme"
+    },
+    {
+        "block": "link",
+        "mod": "theme",
+        "val": "faroe"
+    },
+    {
+        "block": "section",
+        "elem": "subscription2"
+    },
+    {
+        "block": "section",
+        "elem": "main"
+    },
+    {
+        "block": "section",
+        "elem": "layout-text"
+    },
+    {
+        "block": "section",
+        "elem": "quote"
+    },
+    {
+        "block": "section",
+        "elem": "quote",
+        "mod": "important"
+    },
+    {
+        "block": "section",
+        "elem": "quote-content"
+    },
+    {
+        "block": "section",
+        "elem": "paragraph"
+    },
+    {
+        "block": "section",
+        "elem": "paragraph",
+        "mod": "offset"
+    },
+    {
+        "block": "section",
+        "elem": "dropcap"
+    },
+    {
+        "block": "section",
+        "elem": "quote",
+        "mod": "bottom"
+    },
+    {
+        "block": "section",
+        "mod": "offset"
+    },
+    {
+        "block": "section",
+        "mod": "offset",
+        "val": true
+    },
+    {
+        "block": "section",
+        "elem": "header",
+        "mod": "picture",
+        "val": "flight-road"
+    },
+    {
+        "block": "section",
+        "elem": "header",
+        "mod": "picture",
+        "val": "weather"
+    },
+    {
+        "block": "i-bem",
+        "elem": "dom",
+        "mod": "init",
+        "val": "auto"
+    },
+    {
+        "block": "loader",
+        "mod": "type",
+        "val": "js"
+    },
+    {
+        "block": "jquery",
+        "elem": "event",
+        "mod": "type",
+        "val": "pointer"
+    },
+    {
+        "block": "jquery",
+        "elem": "event",
+        "mod": "type",
+        "val": "pointerpressrelease"
+    },
+    {
+        "block": "section",
+        "elem": "header",
+        "mod": "picture",
+        "val": "faroe"
+    },
+    {
+        "block": "section",
+        "elem": "quote",
+        "mod": "important",
+        "val": true
+    },
+    {
+        "block": "section",
+        "elem": "paragraph",
+        "mod": "offset",
+        "val": true
+    },
+    {
+        "block": "section",
+        "elem": "quote",
+        "mod": "bottom",
+        "val": true
+    }
+];
